48cd5562e6be195e43d2a245a62ce8a16bfa9ebb
Patrick <patrick204nqh@gmail.com>
2020-04-28 21:01:58 +0700

    logging users in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { Navbar } from './components/layout/Navbar';
+import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
 import SignIn from './components/auth/SignIn';
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import { SignedInLinks } from './SignedInLinks';
 import { SignedOutLinks } from './SignedOutLinks';
+import { connect } from 'react-redux';
 
-
-export const Navbar = () => {
+const Navbar = () => {
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
@@ -12,7 +12,17 @@ export const Navbar = () => {
           MarioPlan
         </Link>
         <SignedInLinks />
+        <SignedOutLinks />
       </div>
     </nav>
   )
 }
+
+const mapStateToProps = state => {
+  
+  return {
+    
+  }
+}
+
+export default connect(mapStateToProps)(Navbar)
