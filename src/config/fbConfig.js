b1e4c9d7289f051c79da6940911a7274bfa39c3a
Patrick <patrick204nqh@gmail.com>
2020-04-28 14:35:05 +0700

    action create project to firestore

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -16,6 +16,6 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.firestore().settings({ timestampsInSnapshots: true});
-firebase.analytics();
+// firebase.analytics();
 
 export default firebase;
